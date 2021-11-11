8774ce40b4fd9cb4abbf4222c8a6c4250118cc37
kamal-gith <kamaldeenidreez@gmail.com>
2021-11-11 15:23:27 -0800

    made changes.

--- components/Author.jsx
+++ components/Author.jsx
@@ -4,8 +4,11 @@ import Image from 'next/image';
 import { grpahCMSImageLoader } from '../util';
 
 const Author = ({ author }) => (
-  <div className="text-center mt-20 mb-8 p-12 relative rounded-lg bg-black bg-opacity-20">
-    <div className="absolute left-0 right-0 -top-14">
+  <div className="text-center p-12 relative rounded-lg border-b border-gray-400 mb-20">
+    
+    <p>Posted by:</p> <h3 className="text-green-400  mb-2 font-bold">  {author.name}</h3>
+    <p className="text-black text-ls">{author.bio}</p>
+    <div className="mb-8 ">
       <Image
         unoptimized
         loader={grpahCMSImageLoader}
@@ -16,9 +19,8 @@ const Author = ({ author }) => (
         src={author.photo.url}
       />
     </div>
-    <h3 className="text-black mt-4 mb-4 text-xl font-bold">{author.name}</h3>
-    <p className="text-black text-ls">{author.bio}</p>
   </div>
+  
 );
 
 export default Author;
--- components/Header.jsx
+++ components/Header.jsx
@@ -14,21 +14,27 @@ const Header = () => {
 
   return (
     <div className="container mx-auto px-10 mb-8">
-      <div className="border-b w-full inline-block border-blue-400 py-8 ">
+      <div className="border-b w-full inline-block border-gray-200 py-8 ">
         <div className="md:float-left block">
-          <div className='bg-green-500 w-1/2 border-none rounded-lg transform motion-safe:hover:scale-110 mb-4'>
+          <div className='border-none rounded-lg transform motion-safe:hover:scale-110 border-gray-200'>
           <Link href="/">
-            <span className="cursor-pointer font-bold text-3xl text-white ml-1">AsterMira </span>
+            {/* <span className="cursor-pointer font-bold text-3xl text-green-600">Aster Mira </span> */}
+            <span className="font-bold transition duration-500 ease hover:bg-indigo-900 inline-block bg-green-600 text-4xl rounded-sm border-none text-white px-8 py-3 cursor-pointer">Aster Mira </span>
           </Link>
-          </div>
-
-            <p className='text-green-500 font-normal'>Balanced Perspectives to Trending News </p>
-        </div>
+          </div><p className='text-sm mb-2'>Balanced Perspectives to Trending News </p></div>
+        
+        
         <div className="hidden md:float-left md:contents ">
           {categories.map((category, index) => (
             <Link key={index} href={`/category/${category.slug}`}><span className="md:float-right mt-2 align-middle text-blue ml-4 font-semibold cursor-pointer transform motion-safe:hover:scale-110">{category.name}</span></Link>
-          ))}
+            ))}
         </div>
+          <div className="float-middle mt-24"> 
+            <p className='text-red-500 text-sm text-center font-semibold'>
+            Welcome to Aster Mira, best site for all your favorite and trending gists:
+            Politics, Sport, Health, Education, Entertainment, Inspiration and so much more...
+            </p>
+          </div>
       </div>
     </div>
   );
