a3ba26377e4b114c6a866242eb98c7f0b3e2ebaa
kamal-gith <kamaldeenidreez@gmail.com>
2021-11-17 12:10:32 -0800

    made changes.

--- components/Header.jsx
+++ components/Header.jsx
@@ -17,18 +17,18 @@ const Header = () => {
 
   return (
     <div className="container mx-auto px-10 mb-8">
-      <div className="border-b w-full inline-block border-gray-200 py-8 ">
+      <div className="border-b w-full inline-block border-gray-200 py-8 mb-8 ">
         <div className="md:float-left block">
-          <div className='border-none rounded-lg transform motion-safe:hover:scale-110 border-gray-200'>
               <Image
             unoptimized
             alt=''
             href='/'
-            height="50px"
-            width="70px"
+            height="30px"
+            width="170px"
             className="cursor-pointer mt-0 mb-0"
             src={logo}
           />
+          <div className='border-none rounded-lg transform motion-safe:hover:scale-110 border-gray-200'>
           <Link href="/">
            <span className="font-bold transition duration-500 ease inline-block hover:text-red-500 text-2xl rounded-sm border-none text-blue-600 cursor-pointer">ASTERMIRA </span>
           </Link>
