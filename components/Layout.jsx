33885970f11bb45ff2e83d81a2192bb2f631b78b
kamal-gith <kamaldeenidreez@gmail.com>
2022-07-08 18:57:18 +0100

    changes

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -2,10 +2,7 @@ import React from 'react';
 import Banner from './Banner';
 import Footer from './Footer';
 import Header from './Header';
-<<<<<<< HEAD
 import Message from './Message';
-=======
->>>>>>> 89742aa04f09709a7548f48453a5e106ad521a46
 // import WelcomeSection from './WelcomeSection';
 
 const Layout = ({ children }) => (
