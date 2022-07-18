621d53d42ac6989aec2672cef155af3e0099253c
kamal-gith <kamaldeenidreez@gmail.com>
2022-07-18 17:17:25 +0100

    changes

--- components/PostDetail.jsx
+++ components/PostDetail.jsx
@@ -84,7 +84,7 @@ const PostDetail = ({ post }) => {
         <RichText
           content={post.content.raw.children}
           renderers={{
-          code_block: ({ children }) => <div className='mb-8'> <pre className='flex min-h-full text-sm leading-6'><code className='pt-4 rounded-lg pb-4 px-4 bg-black text-yellow-300'>{children}</code></pre></div>,           
+          code_block: ({ children }) => <div className='overflow-auto mb-8 shadow-sm'> <pre className='flex min-h-full text-sm leading-6'><code className='pt-4 rounded-lg pb-4 px-4 bg-black text-green-50'>{children}</code></pre></div>,           
           // img: ({ src, altText, height, width }) => (
             //   <div className='w-full flex justify-center'>
             //     <Image src={src} alt={altText} height={height} width={width} objectFit='cover' />
