c33d9711edbe93b04f405954cdb6cd05307f072c
kamal-gith <kamaldeenidreez@gmail.com>
2022-07-18 01:52:36 +0100

    changes

--- components/PostDetail.jsx
+++ components/PostDetail.jsx
@@ -1,58 +1,66 @@
 import React from 'react';
 import moment from 'moment';
+import { RichText } from '@graphcms/rich-text-react-renderer';
+
 import { FacebookShareButton, FacebookIcon, TelegramShareButton, TelegramIcon, TwitterShareButton, TwitterIcon, WhatsappShareButton, WhatsappIcon,
   LinkedinShareButton, LinkedinIcon, } from 'next-share';
 
 const PostDetail = ({ post }) => {
-  const getContentFragment = (index, text, obj, type) => {
-    let modifiedText = text;
 
-    if (obj) {
-      if (obj.bold) {
-        modifiedText = (<b key={index}>{text}</b>);
-      }
+  // const getContentFragment = (index, text, obj, type) => {
+  //   let modifiedText = text;
 
-      if (obj.italic) {
-        modifiedText = (<em key={index}>{text}</em>);
-      }
+  //   if (obj) {
+  //     if (obj.bold) {
+  //       modifiedText = (<b key={index}>{text}</b>);
+  //     }
+  //     if (obj.bold) {
+  //       modifiedText = (<b key={index}>{text}</b>);
+  //     }
 
-      if (obj.underline) {
-        modifiedText = (<u key={index}>{text}</u>);
-      }
-    }
+  //     if (obj.italic) {
+  //       modifiedText = (<em key={index}>{text}</em>);
+  //     }
 
-    switch (type) {
-      case 'heading-one':
-        return <h1 key={index} className="text-xl font-semibold mb-4">{modifiedText.map((item, i) => <React.Fragment key={i}>{item}</React.Fragment>)}</h1>;
-      case 'paragraph':
-        return <p key={index} className="mb-8">{modifiedText.map((item, i) => <React.Fragment key={i}>{item}</React.Fragment>)}</p>;
-      case 'heading-two':
-        return <h2 key={index} className="text-md font-semibold mb-4">{modifiedText.map((item, i) => <React.Fragment key={i}>{item}</React.Fragment>)}</h2>;
-      case 'heading-three':
-        return <h3 key={index} className="text-md font-semibold mb-4">{modifiedText.map((item, i) => <React.Fragment key={i}>{item}</React.Fragment>)}</h3>;
-      case 'heading-four':
-        return <h4 key={index} className="text-md font-semibold mb-4">{modifiedText.map((item, i) => <React.Fragment key={i}>{item}</React.Fragment>)}</h4>;
-      case 'heading-five':
-        return <h5 key={index} className="text-md font-semibold mb-4">{modifiedText.map((item, i) => <React.Fragment key={i}>{item}</React.Fragment>)}</h5>;
-      case 'heading-six':
-        return <h6 key={index} className="text-md font-semibold mb-4">{modifiedText.map((item, i) => <React.Fragment key={i}>{item}</React.Fragment>)}</h6>;
-      case 'image':
-        return (
-          <img
-            key={index}
-            alt={obj.title}
-            height={obj.height}
-            width={obj.width}
-            src={obj.src}
-          />
-        );
-      default:
-        return modifiedText;
-    }
-  };
+  //     if (obj.underline) {
+  //       modifiedText = (<u key={index}>{text}</u>);
+  //     }
+  //   }
+
+  //   switch (type) {
+  //     case 'heading-one':
+  //       return <h1 key={index} className="text-xl font-semibold mb-4">{modifiedText.map((item, i) => <React.Fragment key={i}>{item}</React.Fragment>)}</h1>;
+  //     case 'paragraph':
+  //       return <p key={index} className="mb-8">{modifiedText.map((item, i) => <React.Fragment key={i}>{item}</React.Fragment>)}</p>;
+  //     case 'heading-two':
+  //       return <h2 key={index} className="text-md font-semibold mb-4">{modifiedText.map((item, i) => <React.Fragment key={i}>{item}</React.Fragment>)}</h2>;
+  //     case 'heading-three':
+  //       return <h3 key={index} className="text-md font-semibold mb-4">{modifiedText.map((item, i) => <React.Fragment key={i}>{item}</React.Fragment>)}</h3>;
+  //     case 'heading-four':
+  //       return <h4 key={index} className="text-md font-semibold mb-4">{modifiedText.map((item, i) => <React.Fragment key={i}>{item}</React.Fragment>)}</h4>;
+  //     case 'heading-five':
+  //       return <h5 key={index} className="text-md font-semibold mb-4">{modifiedText.map((item, i) => <React.Fragment key={i}>{item}</React.Fragment>)}</h5>;
+  //     case 'heading-six':
+  //       return <h6 key={index} className="text-md font-semibold mb-4">{modifiedText.map((item, i) => <React.Fragment key={i}>{item}</React.Fragment>)}</h6>;
+  //     case 'image':
+  //       return (
+  //         <img
+  //           key={index}
+  //           alt={obj.title}
+  //           height={obj.height}
+  //           width={obj.width}
+  //           src={obj.src}
+  //         />
+  //       );
+  //     default:
+  //       return modifiedText;
+  //   }
+  // };
 
   return (
     <>
+
+      
       <div className="bg-white shadow-sm rounded-lg lg:p-8 pb-12 border-t z-10">
         <div className="flex flex-col justify-center relative overflow-hidden shadow-md mb-6 mt-3 ">
         <h1 className="text-center text-capitalize text-lg capitalize font-semibold">{post.title}</h1>
@@ -72,14 +80,39 @@ const PostDetail = ({ post }) => {
           <img src={post.featuredImage.url} alt="" className="mt-10 object-top h-full w-full object-cover  shadow-lg rounded-t-lg lg:rounded-lg" />
         </div>
         <div className="px-4 lg:px-0">
-          {post.content.raw.children.map((typeObj, index) => {
+
+        <RichText
+          content={post.content.raw.children}
+          renderers={{
+          code_block: ({ children }) => <div className=''> <pre className='flex min-h-full text-sm leading-6'><code className='pt-4 pb-4 px-4 bg-blue-900 text-white'>{children}</code></pre></div>,           
+          // img: ({ src, altText, height, width }) => (
+            //   <div className='w-full flex justify-center'>
+            //     <Image src={src} alt={altText} height={height} width={width} objectFit='cover' />
+            //   </div>
+            // ),
+            a: ({ children, href, openInNewTab }) => (<a href={href} target={openInNewTab ? '_blank' : '_self'} rel="noreferrer"  className='underline font-semibold italic'>
+                {children}
+              </a>
+            ),
+            ol: ({ children }) => <ol>{children}</ol>,
+            li: ({ children }) => <li type="1">{children}</li>,
+            p: ({ children }) => <p>{children}</p>,
+            h1: ({ children }) => <h1 className='text-3xl font-bold mb-4 mt-4'>{children}</h1>,
+            h2: ({ children }) => <h2 className='text-2xl font-semibold mb-4 mt-4'>{children}</h2>,
+            h3: ({ children }) => <h3 className='text-xl font-semibold mb-4 mt-4'>{children}</h3>,
+            h4: ({ children }) => <h4 className='text-lg font-semibold mb-4 mt-4'>{children}</h4>,
+            h5: ({ children }) => <h5 className='text-md font-semibold mb-4 mt-4'>{children}</h5>,
+          }}
+        />
+
+          {/* {post.content.raw.children.map((typeObj, index) => {
             const children = typeObj.children.map((item, itemindex) => getContentFragment(itemindex, item.text, item));
 
             return getContentFragment(index, children, typeObj, typeObj.type);
-          })}
+          })} */}
         </div>
         <div className=" flex border-t border-gray-100 align-center w-full justify-center pt-10 "> 
-        <span className=" rounded-md text-sm text-gray-800 mr-2">Kindly Share this story</span>
+        <span className=" rounded-md text-sm text-gray-800 mr-2">Kindly Share this Story</span>
         <div className='flex gap-2'>
           <FacebookShareButton url={'https://astermira.vercel.app/post/'+post.slug}><FacebookIcon size={25} round className='transform motion-safe:hover:scale-110'/></FacebookShareButton>
           <TelegramShareButton url={'https://astermira.vercel.app/post/'+post.slug}><TelegramIcon size={25} round className='transform motion-safe:hover:scale-110'/></TelegramShareButton>
